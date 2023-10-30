4e3cffe1ff4523018834390be0979e68e5ddc001
mikelfere <mikelfere1@gmail.com>
2023-10-29 22:08:34 -0400

    Added document.write() with function calls to calculate charges

--- homework/project8/projects-start-master/chapter08/project1/js/ch08-proj01.js
+++ homework/project8/projects-start-master/chapter08/project1/js/ch08-proj01.js
@@ -6,5 +6,18 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
    going to concern ourselves with minimizing globals, etc */
 
 for(let element of cart){
-   outputCartRow(element, calculateTotal(element.quantity, element.product.price));
+   let total = calculateTotal(element.quantity, element.product.price);
+   outputCartRow(element, total);
 }
+
+document.write("<tr class=\"totals\"><td colspan=\"4\">Subtotal</td><td>$" +
+   subTotal().toFixed(2) + "</td></tr>");
+
+document.write("<tr class=\"totals\"><td colspan=\"4\">Tax</td><td>$" +
+   tax().toFixed(2) + "</td></tr>");
+
+document.write("<tr class=\"totals\"><td colspan=\"4\">Shipping</td><td>$" +
+   shipping().toFixed(2) + "</td></tr>");
+
+document.write("<tr class=\"totals\"><td colspan=\"4\" class=\"focus\">Grand Total</td><td class=\"focus\">$" +
+   total().toFixed(2) + "</td></tr>");
