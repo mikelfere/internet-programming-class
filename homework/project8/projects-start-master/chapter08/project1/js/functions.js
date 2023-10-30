45f2141a6b6b54b11070a99a3375e4d4c3124769
mikelfere <mikelfere1@gmail.com>
2023-10-29 20:29:52 -0400

    Added definitions for calculateTotal() and outputCartRow()

--- homework/project8/projects-start-master/chapter08/project1/js/functions.js
+++ homework/project8/projects-start-master/chapter08/project1/js/functions.js
@@ -1,6 +1,20 @@
 /* define your functions here */
 
+function calculateTotal(quantity, price){
+    return quantity*price;
+}
 
+function outputCartRow(item, total){
+    document.write(
+        "<tr>" +
+            "<td><img src=\"" + item.product.filename + "\"></td>" +
+            "<td>" + item.product.title + "</td>" +
+            "<td>" + item.quantity + "</td>" +
+            "<td>" + item.product.price + "</td>" +
+            "<td>" + (item.product.price * item.quantity) + "</td>" +
+        "</tr>"
+    );
+}
 
 
 
