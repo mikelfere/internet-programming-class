4616af962849b2359b37b19680907d842b26bcc7
mikelfere <mikelfere1@gmail.com>
2023-11-06 09:51:55 -0500

    Events Javascript

---
+++ events-script.js
@@ -0,0 +1,4 @@
+const boxDiv = document.getElementById("box");
+
+//e is an event object that contains information about the event
+boxDiv.addEventListener("click", function(e){alert("hey there");});
