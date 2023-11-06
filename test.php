6eecbdf3c36d9e675af7d5d279337827468b25a3
mikelfere <mikelfere1@gmail.com>
2023-11-06 13:26:34 -0500

    Changed file type

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <script src="script3.js"></script> -->
+    </head>
+    <body>
+        <?php
+            echo("Hello World");
+        ?>
+    </body>
+</html>
