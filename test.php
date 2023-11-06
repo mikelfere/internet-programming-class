879da66def22d9c102ea0e13010bcb5090a7bc46
mikelfere <mikelfere1@gmail.com>
2023-11-06 13:27:39 -0500

    Added comments

--- test.php
+++ test.php
@@ -4,6 +4,7 @@
         <!-- <script src="script3.js"></script> -->
     </head>
     <body>
+        <!-- An html file has to be saved as .php to run php code inside of it -->
         <?php
             echo("Hello World");
         ?>
